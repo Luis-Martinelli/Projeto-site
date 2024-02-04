672bc068f55aa58e0fc8fbde61f928fe6fe6388c
Luis-Martinelli <lfnm1989@gmail.com>
2024-02-03 23:00:53 -0300

    dialog

--- js/script.js
+++ js/script.js
@@ -3,21 +3,32 @@ const drop = document.querySelector('.dropdown');
 const menu = document.getElementById('dropdown');
 const ico = document.querySelector('.ico');
 
+// Ativar itens do menu
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add('ativo');
+  }
+}
+links.forEach(ativarLink);
+
 menu.onclick = () => {
   drop.classList.toggle('ativo');
   ico.classList.toggle('ativo');
 };
 //Login
 const showLogin = document.getElementById('show');
-const login = document.querySelector('.popup');
+const login = document.querySelector('dialog');
 const close = document.querySelector('.close-btn');
 
 showLogin.onclick = () => {
-  login.classList.add('active');
+  login.showModal();
 };
-
 close.onclick = () => {
-  login.classList.remove('active');
+  login.close();
 };
 
 //Menu-footer
