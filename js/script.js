8964f86f2d670aefb8a1c389cc93b00c74515a89
Luis-Martinelli <lfnm1989@gmail.com>
2024-09-15 23:07:17 -0300

    keycode=13 & destaques

--- js/script.js
+++ js/script.js
@@ -58,6 +58,12 @@ function checkChar(e) {
     return true;
   }
 }
+document.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    validar();
+  }
+});
 
 function validar(e) {
   const idDejem = document.getElementById('idDejem').value;
@@ -132,4 +138,19 @@ function nextImage() {
     count = 1;
   }
   document.getElementById('radio' + count).checked = true;
+}
+
+//Mascara
+function mascara(i) {
+  var v = i.value;
+
+  if (isNaN(v[v.length - 1])) {
+    // impede entrar outro caractere que não seja número
+    i.value = v.substring(0, v.length - 1);
+    return;
+  }
+
+  i.setAttribute('maxlength', '14');
+  if (v.length == 3 || v.length == 7) i.value += '.';
+  if (v.length == 11) i.value += '-';
 }
